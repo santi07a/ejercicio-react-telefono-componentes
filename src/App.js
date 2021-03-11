f574e7369dd98d416792de88848ec821ea98937c
Mario González <mario@mariogl.com>
2021-03-11 11:40:21 +0100

    Traído HTML y CSS de la maqueta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      {/* <!-- El siguiente elemento se oculta añadiéndole la clase "off" --> */}
+      <span className="mensaje">Llamando...</span>
+      <main className="telefono">
+        <div className="botones">
+          <ol className="teclado">
+            <li><button>1</button></li>
+            <li><button>2</button></li>
+            <li><button>3</button></li>
+            <li><button>4</button></li>
+            <li><button>5</button></li>
+            <li><button>6</button></li>
+            <li><button>7</button></li>
+            <li><button>8</button></li>
+            <li><button>9</button></li>
+            <li><button>0</button></li>
+            <li><button className="big">borrar</button></li>
+          </ol>
+        </div>
+        <div className="acciones">
+          <span className="numero">667359961</span>
+          {/* <!-- El botón de llamar debe tener la clase "activo" cuando -->
+                <!-- el número de teléfono tiene 9 cifras --> */}
+          <a href="llamar" className="llamar">Llamar</a>
+          {/* <!-- Sólo se tiene que ver un botón u otro --> */}
+          <a href="colgar" className="colgar activo">Colgar</a>
+        </div>
+      </main>
     </div>
   );
 }
